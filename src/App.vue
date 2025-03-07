fd3ba473a5c17d677572b88e961219d421f62ec3
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2025-03-07 16:14:20 +0400

    firist-commit
    07/03/2025

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="black-square"></div>
+  </div>
+</template>
+
+<style>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.black-square {
+  width: 200px;
+  height: 200px;
+  background-color: black;
+}
+</style>
